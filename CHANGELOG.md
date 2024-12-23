b990499b6751ac87725c1d95daf15332d6ca0363
Liu Xue Yan <liu_xue_yan@foxmail.com>
2024-12-23 10:09:29 +0800

    docs(changelog): update CHANGELOG.md for v0.2.2 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,29 @@
 # Changelog
 
+## v0.2.2
+
+> 📅 2024-12-23
+
+- ✨ **New Features:**
+  - New `fifo-t` (Timestamp based pseudo FIFO replacement policy)
+
+- 🐛 **Bug Fixes:**
+  - Wrong type checking for `redis.cluster.RedisCluster` and `redis.asyncio.cluster.RedisCluster`
+
+- 🛠 **Improvements:**
+  - Remove some un-used utilities
+
+- 📦 **Packaging:**
+  - Add `hiredis` extras requirements
+  - Adjust `manifest.in` file
+
+- 📚 **Documentation:**
+  - Modify documentation for docker-composed unit testing
+
+- 🧪 **Tests:**
+  - New add cluster based tests
+  - Improved docker-composed based tests
+
 ## v0.2.1
 
 > 📅 2024-12-19
--- tests/test_cluster_multiple.py
+++ tests/test_cluster_multiple.py
@@ -1,6 +1,6 @@
 from os import getenv
 from random import randint
-from unittest import TestCase
+from unittest import TestCase, skipUnless
 
 from redis.cluster import ClusterNode, RedisCluster
 
@@ -14,23 +14,25 @@ from redis_func_cache import (
     RrClusterMultiplePolicy,
 )
 
-REDIS_CLUSTER_NODES = getenv("REDIS_CLUSTER_NODES", "127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003")
-CLUSTER_NODES = [
-    ClusterNode(cluster.split(":")[0], int(cluster.split(":")[1])) for cluster in REDIS_CLUSTER_NODES.split()
-]
-REDIS_CLIENT: RedisCluster = RedisCluster(startup_nodes=CLUSTER_NODES)
+REDIS_CLUSTER_NODES = getenv("REDIS_CLUSTER_NODES")
+if REDIS_CLUSTER_NODES:
+    CLUSTER_NODES = [
+        ClusterNode(cluster.split(":")[0], int(cluster.split(":")[1])) for cluster in REDIS_CLUSTER_NODES.split()
+    ]
+    REDIS_CLIENT: RedisCluster = RedisCluster(startup_nodes=CLUSTER_NODES)
 
-MAXSIZE = 8
-CACHES = {
-    "tlru": RedisFuncCache(__name__, LruTClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
-    "lru": RedisFuncCache(__name__, LruClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
-    "mru": RedisFuncCache(__name__, MruClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
-    "rr": RedisFuncCache(__name__, RrClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
-    "fifo": RedisFuncCache(__name__, FifoClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
-    "lfu": RedisFuncCache(__name__, LfuClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
-}
+    MAXSIZE = 8
+    CACHES = {
+        "tlru": RedisFuncCache(__name__, LruTClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
+        "lru": RedisFuncCache(__name__, LruClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
+        "mru": RedisFuncCache(__name__, MruClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
+        "rr": RedisFuncCache(__name__, RrClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
+        "fifo": RedisFuncCache(__name__, FifoClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
+        "lfu": RedisFuncCache(__name__, LfuClusterMultiplePolicy, client=REDIS_CLIENT, maxsize=MAXSIZE),
+    }
 
 
+@skipUnless(REDIS_CLUSTER_NODES, "REDIS_CLUSTER_NODES environment variable is not set")
 class ClusterMultipleTest(TestCase):
     def setUp(self):
         for cache in CACHES.values():
