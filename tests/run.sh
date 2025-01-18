22084aa18cd7a7d318d7fb6009883c9ddf08cf10
tanbro <tanbro@163.com>
2025-01-19 00:38:23 +0800

    build: modernize and optimize project build and test流程
    - 更新 pyproject.toml，增加新的依赖组和开发依赖
    - 修改 .readthedocs.yaml，调整文档构建配置
    - 精简 requirements.txt，移除不必要的依赖
    - 更新 tests/requirements.txt，移除 coverage 依赖
    - 重构 tests/run.sh，使用 uv 工具替代手动安装依赖和运行测试
    
    此提交将项目构建和测试流程现代化，提高了开发效率和代码质量。

--- tests/run.sh
+++ tests/run.sh
@@ -1,9 +1,7 @@
 set -e
 
 export SETUPTOOLS_SCM_PRETEND_VERSION=0
-export PIP_DISABLE_PIP_VERSION_CHECK=1
-export PIP_ROOT_USER_ACTION=ignore
-export PIP_NO_WARN_SCRIPT_LOCATION=1
+
 
 PYTHON_LIST=(python3.8 python3.9 python3.10 python3.11 python3.12 python3.13)
 for PYTHON in ${PYTHON_LIST[@]}
@@ -15,18 +13,20 @@ do
     echo
     TMPDIR=$(mktemp -d)
     trap 'rm -rf $TMPDIR' EXIT
-    $PYTHON -m venv $TMPDIR
+    uv venv --python ${PYTHON} $TMPDIR
     echo
-    $TMPDIR/bin/python -Bm pip install --no-compile -e .[all,types-all] -r tests/requirements.txt ruff mypy pytest pytest-cov
+    source $TMPDIR/bin/activate
     echo
-    echo "Linting:"
-    $TMPDIR/bin/python -Bm ruff check
+    uv sync --group lint --group static-type-check --group pytest
     echo
-    echo "Static type checking:"
-    $TMPDIR/bin/python -Bm mypy
+    echo "Lint:"
+    ruff check
     echo
-    echo "Unit test with coverage:"
-    $TMPDIR/bin/python -Bm pytest --cov --cov-report=xml --junitxml=junit.xml
+    echo "Static type check:"
+    mypy
+    echo
+    echo "Py test:"
+    pytest --cov --cov-report=xml --junitxml=junit.xml
     echo
     echo "*****************************************************************"
     echo "End of ${PYTHON} unit-test"
